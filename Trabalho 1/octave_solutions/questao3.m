a28bbe9ab2369f13c94a7e0ad6c73acc40a04e68
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2022-09-25 16:07:27 -0300

    questão 3 traçada

---
+++ Trabalho 1/octave_solutions/questao3.m
@@ -0,0 +1,14 @@
+% Programa da segunda questao do trabalho de sinais e sistemas
+% 2022.2
+
+% Intervalos
+dt=0.001;
+
+% Dados basicos
+t=-2:dt:2-dt; x=1-exp(abs(t));
+plot(t, x, "r", "linewidth", 3);
+
+title("Grafico de x(t) por t - Questao 3", "fontsize", 20);
+xlabel("t", "fontsize", 18);
+ylabel("x(t)", "fontsize", 18);
+
