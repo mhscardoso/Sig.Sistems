906f4d7fb30045a45d5fce4b46fbf54bfe65b634
Matheus Cardoso <mhcardoso@poli.ufrj.br>
2022-09-27 19:07:25 -0300

    ending question 4 (thanks God)

--- Trabalho 1/octave_solutions/questao3/xk.m
+++ Trabalho 1/octave_solutions/questao3/xk.m
@@ -5,7 +5,6 @@ t = -2:dt:2-dt;
 x = (1 - exp(abs(t)));
 
 Xk = zeros(1, 7);
-
 for k = -3:1:3;
   Xk(k + 4) = ((2 - 2*exp(2)*cos(k * pi))/(4 + (k*pi)^2)) + sinc(k);
 end
@@ -13,7 +12,6 @@ end
 disp(Xk);
 
 % Magnitude
-
 M = zeros(1, 7);
 for k = 1:1:7;
   M(k) = abs(Xk(k));
@@ -22,7 +20,6 @@ end
 disp(M);
 
 % Potencia
-
 P = zeros(1, 4);
 for k = 1:1:4;
   P(k) = M(k + 3)^2;
@@ -33,7 +30,13 @@ end
 
 disp(P);
 
-disp("Soma ate 3: "), disp(sum(P));
+disp("Potencia acumulada:"), disp(sum(P));
+
+% Potencia total
+P_total = (7 - 4*exp(2) + exp(4))/4;
+
+% Potencias relativas
+P_rel = P/P_total
 
 
 
