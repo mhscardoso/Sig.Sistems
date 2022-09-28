2c42d4c4bdb061d90ef4fb4bfd96878c84170940
Matheus Cardoso <mhcardoso@poli.ufrj.br>
2022-09-27 21:01:27 -0300

    end

--- Trabalho 1/octave_solutions/questao5/a.m
+++ Trabalho 1/octave_solutions/questao5/a.m
@@ -1,17 +1,17 @@
 %% Programa da quinta questao do trabalho de Sinais e Sistemas
-## 2022.2
+%% 2022.2
 
 % dados basicos
-f0=132; soma=0;
+f0=156; soma=0;
 dt=1/2000;
-t=0:dt:5;
-var=2*pi*f0*t;
+t=0:dt:5-dt;
+var=2*pi*f0.*t;
 
 % Gerando o sinal
-soma = 0;
-for i=0:1:5;
-  a=2*i+1;
-  xq=sin(a*var)/a;
+for i=0:1:2;
+  sa=2*i+1;
+  xq=sin(sa*var)/sa;
   soma=soma+xq;
 end
-sound(soma);
+
+plot(t, soma)
