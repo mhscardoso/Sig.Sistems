8d21b5595ff220ed2b8ded8f2b7b1c85845e2980
Matheus Cardoso <mhcardoso@poli.ufrj.br>
2022-12-10 14:18:51 -0300

    Question 1 - corrections

--- Trabalho 4/octave_solutions/teste.m
+++ Trabalho 4/octave_solutions/teste.m
@@ -6,23 +6,38 @@ C = [1 0 0];
 pkg load control
 
 %% Sistema de espaço de estados
-sys = ss(A, B, C);
+sp = ss(A, B, C);
 
 %% Funcao de transferencia
-SYS = tf(sys);
+SYS = tf(sp);
 
 %% Resposta ao Degrau
-[Y, T, X] = step(SYS);
+[Y_degrau, T_degrau, X_degrau] = step(sp);
+%% Plotagem
+%plot(T_degrau, Y_degrau, "r", "linewidth", 3), title("Y x T - Resposta ao Degrau"), xlabel("t"), ylabel("y")
+%plot(T_degrau, X_degrau, "linewidth", 3), title("X x T - Resposta ao Degrau"), xlabel("t"), ylabel("x")
 
-%% Plotando Y
-%plot(T, Y);
-%title("y X t - Resposta ao Degrau");
-%xlabel("t");
-%ylabel("y");
+%% REN
+%% x0 como multiplo de V:
+lambda1 = eig(A)(1)
+x0_1 = [1 ; lambda1 ; lambda1^2]
+[Y_ren_v, T_ren_v, X_ren_v] = initial(sp, x0_1)
+
+%% Plotagem
+%plot(T_ren_v, Y_ren_v, "r", "linewidth", 3), title("Y x T - REN (x0 = \lambda V)"), xlabel("t"), ylabel("y")
+%plot(T_ren_v, X_ren_v, "linewidth", 3), title("X x T - REN (x0 = \lambda V)"), xlabel("t"), ylabel("x")
+
+%% REN
+%% x0 como multiplo das colunas de W:
+sigma = (eig(A)(2) + eig(A)(3)) / 2       %% Parte real
+omega = (eig(A)(2) - eig(A)(3)) / (2 * i) %% Parte imaginaria
+x0_2 = [1 ; sigma + omega ; (sigma^2 - omega^2) + 2 * sigma * omega]
+[Y_ren_w, T_ren_w, X_ren_w] = initial(sp, x0_2)
+
+%% Plotagem
+%plot(T_ren_w, Y_ren_w, "r", "linewidth", 3), title("Y x T - REN (x0 = C.L de W)"), xlabel("t"), ylabel("y")
+plot(T_ren_w, X_ren_w, "linewidth", 3), title("X x T - REN (x0 = C.L de W)"), xlabel("t"), ylabel("x")
 
 
-%% Plotando X
-%plot(T, X);
-%title("x X t - Resposta ao Degrau");
-%xlabel("t");
-%ylabel("x");
+
+
