54de759305a98c19b8b340d6699bb9dce21698aa
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2022-12-10 18:06:58 -0300

    questões e, g, h, i

--- Trabalho 4/octave_solutions/questao2.m
+++ Trabalho 4/octave_solutions/questao2.m
@@ -1,5 +1,14 @@
-%%% Testes
 %% Testes
 A = [0 1 0 0 ; -8 0 4 0 ; 0 0 0 1 ; 4 0 -8 0 ];
 B = [0 ; 0 ; 0 ; 0];
-C = [1 0 1 0];
+C = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
+
+pkg load control
+
+%% Sistema de espaço de estados
+sp = ss(A, B, C, "outname", {"y1", "dy1/dt", "y2", "dy2/dt"});
+
+x0 = [4 ; 0 ; 0 ; 0];
+
+initial(sp, x0)
+
