bd56f6cc57088905349450725bc6959f6eed0f21
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2023-01-03 16:47:39 -0300

    Questao 4c

--- Trabalho 5/octave_solutions/questao4a.m
+++ Trabalho 5/octave_solutions/questao4a.m
@@ -4,7 +4,7 @@
 dt=0.001;
 
 % Dados basicos
-t=0:dt:12-dt; y=e.^(2/3*t).*(3/4*cos(4*sqrt(2)/3*t)-15*sqrt(2)/16*sin(4*sqrt(2)/3*t))-3/4;
+t=0:dt:12-dt; y=e.^(2/3*t).*(-3/4*cos(4*sqrt(2)/3*t)-15*sqrt(2)/16*sin(4*sqrt(2)/3*t))+3/4;
 plot (t, y, "r", "linewidth", 3);
 title("y(t) por t - 4.a)", "fontsize", 20);
 xlabel("t", "fontsize", 18);
