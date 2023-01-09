977e4e2ce32d5e5f242101b1a4d2a2c8bdb03e76
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2023-01-09 16:23:42 -0300

    Revisar a 1e

--- Trabalho 6/octave_solutions/questao1c.m
+++ Trabalho 6/octave_solutions/questao1c.m
@@ -1,14 +1,14 @@
 dt=0.4;
 t=-2:dt:2;
 v=exp(-2*t.^2);
-% plot (t, v, "rx", "linewidth", 10);
-% title("v(t) por t - 1.c)", "fontsize", 20);
-% xlabel("t", "fontsize", 18);
-% ylabel("v(t)", "fontsize", 18);
+%plot (t, v, "r*-", "linewidth", 3);
+%title("v(t) por t - 1.c)", "fontsize", 20);
+%xlabel("t", "fontsize", 18);
+%ylabel("v(t)", "fontsize", 18);
 
 f=-1.25:(1/4):1.25;
 V = fft(v)
 V = fftshift(V);
 modV = abs(V);
-plot(f, modV, "r*")
+plot(f, modV, "r*-", "linewidth", 3)
 title("Espectro de magnitudes", "fontsize", 20);
--- Trabalho 6/octave_solutions/questao1d.m
+++ Trabalho 6/octave_solutions/questao1d.m
@@ -1,20 +1,15 @@
-%Questão 1.d)
-
-% Intervalo
 dt=0.2;
-
-% Dados basicos
 t=-2:dt:2;
 v=exp(-2*t.^2);
-%plot (t, v, "rx", "linewidth", 10);
+%plot (t, v, "r*-", "linewidth", 3);
 %title("v(t) por t - 1.d)", "fontsize", 20);
-%label("t", "fontsize", 18);
+%xlabel("t", "fontsize", 18);
 %ylabel("v(t)", "fontsize", 18);
 
 f=-2.5:(1/4):2.5;
 V = fft(v)
 V = fftshift(V);
 modV = abs(V);
-plot(f, modV, "r*")
+plot(f, modV, "r*-", "linewidth", 3)
 title("Espectro de magnitudes", "fontsize", 20);
 
