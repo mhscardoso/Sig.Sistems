32b787e0c52bc39945c41815632b6376cfb6ed32
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2023-01-12 20:30:10 -0300

    Terminando a questão 3

--- Trabalho 6/octave_solutions/questao3b.m
+++ Trabalho 6/octave_solutions/questao3b.m
@@ -1,5 +1,5 @@
-dt=0.001;
-T0=5;
+dt=0.05;
+T0=10;
 t=-(T0/2):dt:(T0/2);
 
 p=4;
@@ -8,11 +8,14 @@ c=2;
 
 x=4*(abs(t)<=p)+(pi/2-(pi/(2*r))*abs(t-2)).*(abs(t-2)<=r)+sqrt(c^2-(t+2).^2).*(abs(t+2)<=c);
 
+f=-1/(2*dt):(1/T0):1/(2*dt);
 X = fft(x)
 X = fftshift(X);
 modX = abs(X);
 angX = angle(X);
-plot(f, modX, "r*-", "linewidth", 3)
+subplot(2, 1, 1)
+plot(f, modX, "linewidth", 3)
 title("Espectro de magnitudes", "fontsize", 20);
-plot(f, angX, "r*-", "linewidth", 3)
+subplot(2, 1, 2)
+plot(f, angX, "linewidth", 3)
 title("Espectro de fases", "fontsize", 20);
