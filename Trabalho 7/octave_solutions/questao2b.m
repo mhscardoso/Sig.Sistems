e7dfdab4aa6f593dce0ccb72d25d87d594cacfee
Matheus Cardoso <mhcardoso@poli.ufrj.br>
2023-01-20 21:11:44 -0300

    Work 7 - ...

--- Trabalho 7/octave_solutions/questao2b.m
+++ Trabalho 7/octave_solutions/questao2b.m
@@ -1,18 +1,30 @@
-%% Dados basicos
-dt = 0.001;
+%% dados basicos
+T0 = 10;
+N = 1024;
+dt = T0 / (N - 1);
+df = 1 / T0;
+fa = 1 / dt;
 
-t1 = -4:dt:0;
-t2 = 0:dt:2;
-t3 = 2:dt:4;
 
-x1 = 4 - sqrt(-t1.^2 - 4 * t1);
-x2 = 4 + t2 * (pi / 2);
-x3 = 4 + 2 * pi - t3 * (pi / 2);
+t = -T0 / 2 : dt : T0 / 2;
+f = -fa / 2 : df : fa / 2;
 
-t = [t1 t2 t3];
-x = [x1 x2 x3];
-
-%% plot(t, x);
+%% p_4(t) + r_2(t - 2) - c_2(t + 2)
+x = 4*(abs(t) <= 4) + \
+    (pi / 2 - (pi / 4) * abs(t - 2)).*(abs(t - 2) <= 2) - \
+    (sqrt(4 - (t + 2).^2)) .* (abs(t + 2) <= 2);
 
 X = fft(x);
+Xa = fftshift(X);
+
+modX = abs(Xa);
+angleX = angle(Xa);
+
+subplot(2, 1, 1)
+plot(f, modX, "linewidth", 3)
+title("Espectro de magnitudes", "fontsize", 20);
+
+subplot(2, 1, 2)
+plot(f, angleX, "linewidth", 3)
+title("Espectro de fases", "fontsize", 20);
 
